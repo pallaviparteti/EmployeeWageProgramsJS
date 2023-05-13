950d487f9fac2120cebbe96a43d7f2be05c2cd49
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-05-13 09:42:26 +0530

    Add: to calculate the wages for months using fro loop

--- EmployeeWageProblems.js
+++ EmployeeWageProblems.js
@@ -1,25 +1,30 @@
 console.log("Welcome To Employee Wage Problems");
-//UC3
-const IS_NO_TIME = 0;
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const PART_TIME_HOURS = 4;
-const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOURS = 20;
-function getWorkingHours(empCheck){
-switch (empCheck){
-    case IS_PART_TIME :
-        empHrs = PART_TIME_HOURS;
-        return PART_TIME_HOURS;
-        case IS_FULL_TIME :
-            empHrs = FULL_TIME_HOURS;
-          return FULL_TIME_HOURS;
-            default :
-            return 0;
-}
-}
-let empHrs = 0;
-let empCheck = Math.floor(Math.random()*10) % 3;
-empHrs = getWorkingHours(empCheck);
-let employeeWage = empHrs * WAGE_PER_HOURS;
-console.log("Employee daily wage :" + employeeWage);
+
+    //UC4
+
+    const IS_NO_TIME = 0;
+    const IS_PART_TIME = 1;
+    const IS_FULL_TIME = 2;
+    const PART_TIME_HOURS = 4;
+    const FULL_TIME_HOURS = 8;
+    const WAGE_PER_HOURS = 20;
+    const NUMBER_OF_WORKING_DAYS = 20;
+    function getWorkingHours(empCheck){
+    switch (empCheck){
+        case IS_PART_TIME :
+            empHrs = PART_TIME_HOURS;
+            return PART_TIME_HOURS;
+            case IS_FULL_TIME :
+                empHrs = FULL_TIME_HOURS;
+              return FULL_TIME_HOURS;
+                default :
+                return 0;
+    }
+    }
+    let empHrs = 0;
+    for(let day = 0; day < NUMNER_OFWORKING_DAYS; day++){
+    let empCheck = Math.floor(Math.random()*10) % 3;
+    empHrs+= getWorkingHours(empCheck);
+    }
+    let employeeWage = empHrs * WAGE_PER_HOURS;
+    console.log("Total Hours :" +empHrs+ "Employee Daily Wage" employeeWage);
