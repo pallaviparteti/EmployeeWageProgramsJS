9f3a045325c5d5e3cd90041364facd831b122aef
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-05-13 09:25:37 +0530

    Add:calculate employee daily wage

--- EmployeeWageProblems.js
+++ EmployeeWageProblems.js
@@ -1,13 +1,23 @@
 console.log("Welcome To Employee Wage Problems");
-//UC1
-const IS_ABSENT = 0;
-let empCheck = Math.floor(Math.random()*10) % 2;
-if(empCheck = IS_ABSENT){
-    console.log("Employee is absent");
-    return;
+//UC2
+const IS_NO_TIME = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOURS = 20;
+let empHrs = 0;
+let empCheck = Math.floor(Math.random()*10) % 3;
+switch (empCheck){
+    case IS_PART_TIME :
+        empHrs = PART_TIME_HOURS;
+        break;
+        case IS_FULL_TIME :
+            empHrs = IS_FULL_TIME;
+            break;
+            default :
+            empHrs = IS_NO_TIME;
 }
-else
-{
-console.log("Employee is present");
-return;
-}
+let employeeWage = empHrs * WAGE_PER_HOURS;
+console.log("Employee daily wage :" + employeeWage);
+
